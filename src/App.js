f80a9338aaa8d8ba320fb7da97fbec6a71ae164d
Paula-Lavagna <paulalavagna@gmail.com>
2021-04-14 14:53:01 -0300

    cambio en app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! Bye!! Hi again
+          Hi there! Bye!!
         </p>
         <a
           className="App-link"
