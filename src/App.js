3dbf3eeb133768f656dd2d53bbed6eb6aa882910
Paula-Lavagna <paulalavagna@gmail.com>
2021-04-14 15:12:30 -0300

    cambio en app.js a ver si travis

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! Bye!!
+          Hi there! Bye!! a veeer
         </p>
         <a
           className="App-link"
